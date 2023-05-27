ab8f952390bc732cb012d47bc4fdf6e631214592
Aravind Samala <aravind.samala96@gmail.com>
2023-05-27 17:52:08 +0530

    Add key prop to the iterator and change the prisma query to findunique movie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Header from "@/components/header";
 import MovieCard from "@/components/Moviecard";
 import { prisma } from "../lib/server";
 import Link from "next/link";
+import Image from "next/image";
+import { randomUUID } from "crypto";
 async function getMovies() {
   const movies = await prisma.movies.findMany({
     take: 10,
@@ -16,7 +18,7 @@ export default async function Home() {
       <Header />
       <main>
         <div className="w-4/5 mx-auto py-4">
-          <img
+          <Image
             src="bms_offer.avif"
             alt="Bookmyshow offer indicating 2 free Movie tickets upon conditions"
             className="object-cover"
@@ -25,7 +27,7 @@ export default async function Home() {
         <section>
           <div className="flex gap-4 ">
             {movies.map((movie) => (
-              <Link href={`movies/${movie.id}`}>
+              <Link href={`movies/${movie.id}`} key={randomUUID()}>
                 <MovieCard
                   name={movie.name}
                   movie_length={movie.movie_length}
